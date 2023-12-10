633f09e00905dbe0babc78721ab2ff91c9133805
clraho <58389097+clraho@users.noreply.github.com>
2023-12-10 05:13:51 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const ip = `127.0.0.1`; // changer et mettre ip publique ec2 
+const port = 3000;
+
+// Définir le dossier public pour les fichiers statiques (comme les fichiers HTML, CSS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Endpoint pour afficher le fichier HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`http://${ip}:${port}`);
+});
