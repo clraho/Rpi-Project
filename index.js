b677451d0b6fbd695bf334f8365c2f9fee8043f3
clraho <58389097+clraho@users.noreply.github.com>
2023-12-11 10:52:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const ip = `51.21.86.169`; // changer et mettre ip publique ec2 
+const ip = `127.0.0.1`; // changer et mettre ip publique ec2 
 const port = 3000;
 
 // Définir le dossier public pour les fichiers statiques (comme les fichiers HTML, CSS, etc.)
