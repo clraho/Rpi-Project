8aae9799cee2d3dca56cbdf60738a5786efb781b
clraho <58389097+clraho@users.noreply.github.com>
2023-12-20 16:56:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const { timeStamp } = require('console');
 const app = express();
 const ip = `127.0.0.1`; // changer et mettre ip publique ec2 
 const port = 3000;
 
-// Définir le dossier public pour les fichiers statiques (comme les fichiers HTML, CSS, etc.)
-app.use(express.static(path.join(__dirname, 'public')));
+const connectedRaspberries = new Map();
 
-// Endpoint pour afficher le fichier HTML
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.post('/update-ip', (req, res) => {
+  const { ip } = req.body;
+  connectedRaspberries.set(ip, Date.now());   //date.now permet d'avoir le timestamp du moment où on a une requête
+  console.log(`IP du Raspberry mise à jour : ${ip}`);
+  console.log(connectedRaspberries);
+  res.send('OK');
+});
+
+app.get('/raspberry-status', (req, res) => {
+  const raspberries = Array.from(connectedRaspberries);
+  res.json({ raspberries });
+});
+
 app.listen(port, () => {
-    console.log(`http://${ip}:${port}`);
+  console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
 });
